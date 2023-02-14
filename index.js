008228e981e4a3a1082beb1c29236e3653c38c6d
Evgenija26 <98258381+Evgenija26@users.noreply.github.com>
2023-02-14 21:55:52 +0200

    .

---
+++ index.js
@@ -0,0 +1,54 @@
+const contacts = require("./contacts");
+// const argv = require("yargs").argv;
+
+// function invokeAction({ action, id, name, email, phone }) {
+//   switch (action) {
+//     case "list":
+//       contacts.listContacts();
+//       break;
+
+//     case "get":
+//       contacts.getContactById(id);
+//       break;
+
+//     case "add":
+//       contacts.addContact(name, email, phone);
+//       break;
+
+//     case "remove":
+//       contacts.removeContact(id);
+//       break;
+
+//     default:
+//       console.warn("\x1B[31m Unknown action type!");
+//   }
+// }
+
+// invokeAction(argv);
+
+const argv = require("yargs").argv;
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
+
+    case "get":
+      getContactById(id);
+      break;
+
+    case "add":
+      addContact(name, email, phone);
+      break;
+
+    case "remove":
+      removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
